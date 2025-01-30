34002edb9e076391bf20c40c79504ca7914f873e
Sankar Manoj <smanoj@tenstorrent.com>
2025-01-30 11:31:25 +0000

    #0: Fix device weights

--- tests/ttnn/unit_tests/operations/test_new_conv2d.py
+++ tests/ttnn/unit_tests/operations/test_new_conv2d.py
@@ -50,6 +50,7 @@ def run_conv(
     input_mesh_mapper=None,
     weight_mesh_mapper=None,
     output_mesh_composer=None,
+    preprocess_weights_on_device=True,
 ):
     if isinstance(device, ttnn.MeshDevice):
         assert input_mesh_mapper is not None, "Expected mesh mapper for input tensor when using device mesh"
@@ -131,6 +132,7 @@ def run_conv(
         enable_split_reader=False,
         enable_subblock_padding=False,
         output_layout=output_layout,
+        preprocess_weights_on_device=preprocess_weights_on_device,
     )
     compute_config = ttnn.init_device_compute_kernel_config(
         device.arch(),
@@ -1324,6 +1326,7 @@ def test_sd_conv(
         shard_layout=shard_layout,
         fp32_accum=fp32_accum,
         packer_l1_acc=True,
+        preprocess_weights_on_device=True,
     )
 
 
@@ -2644,6 +2647,7 @@ def test_non_tile_multiple_height_conv_wh(
         fp32_accum=fp32_accum,
         has_bias=has_bias,
         output_layout=ttnn.ROW_MAJOR_LAYOUT,
+        preprocess_weights_on_device=True,
     )
 
 
@@ -2725,6 +2729,7 @@ def test_non_tile_multiple_width_conv_wh(
         config_override,
         use_shallow_conv_variant=(input_channels == 16),
         output_layout=ttnn.ROW_MAJOR_LAYOUT,
+        preprocess_weights_on_device=True,
     )
 
 
