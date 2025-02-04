d9e8a84d84c95b49d327394ee43bdabf84750531
Nathan Sidwell <nsidwell@tenstorrent.com>
2025-02-04 17:00:34 +0000

    Revert "#13415: Update left shift (#15538)"
    This reverts commit 7b74c5bf893fe34964589547fb6792ae2d1db940.

--- tt_metal/hw/ckernels/blackhole/metal/llk_api/llk_sfpu/ckernel_sfpu_left_shift.h
+++ tt_metal/hw/ckernels/blackhole/metal/llk_api/llk_sfpu/ckernel_sfpu_left_shift.h
@@ -17,9 +17,13 @@ template <bool APPROXIMATION_MODE, int ITERATIONS = 8>
 inline void calculate_left_shift(const uint shift_amt) {
 #pragma GCC unroll 0
     for (int d = 0; d < ITERATIONS; d++) {
-        TTI_SFPLOAD(0,12,ADDR_MOD_7,0);
-        TT_SFPSHFT(shift_amt,0,0,1);
-        TTI_SFPSTORE(0,12,ADDR_MOD_7,0);
+        vInt val = dst_reg[0];
+        vInt v = val;
+
+        val = val << shift_amt;
+        val = setsgn(val, v);
+        dst_reg[0] = val;
+
         dst_reg++;
     }
 }
