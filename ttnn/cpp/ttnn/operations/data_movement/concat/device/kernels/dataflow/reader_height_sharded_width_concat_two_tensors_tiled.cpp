a938b4081a9f485caa6ad561878cdfcd4815e551
Evan Smal <esmal@tenstorrent.com>
2025-02-05 19:16:34 +0000

    Works with groups

--- ttnn/cpp/ttnn/operations/data_movement/concat/device/kernels/dataflow/reader_height_sharded_width_concat_two_tensors_tiled.cpp
+++ ttnn/cpp/ttnn/operations/data_movement/concat/device/kernels/dataflow/reader_height_sharded_width_concat_two_tensors_tiled.cpp
@@ -14,10 +14,10 @@ void kernel_main() {
     constexpr uint32_t input1_num_tiles_height = get_compile_time_arg_val(5);
     constexpr uint32_t input1_num_tiles_width = get_compile_time_arg_val(6);
     constexpr uint32_t tile_size = get_compile_time_arg_val(7);
-    // constexpr uint32_t groups = get_compile_time_arg_val(8);
+    constexpr uint32_t groups = 2;  // get_compile_time_arg_val(8);
 
-    constexpr uint32_t input0_stride = tile_size * input0_num_tiles_width;
-    constexpr uint32_t input1_stride = tile_size * input1_num_tiles_width;
+    constexpr uint32_t input0_stride = tile_size * input0_num_tiles_width / groups;
+    constexpr uint32_t input1_stride = tile_size * input1_num_tiles_width / groups;
 
     const uint32_t base_l1_read_addr_0 = get_read_ptr(input0_cb);
     const uint64_t noc_addr_0 = get_noc_addr(base_l1_read_addr_0);
@@ -31,21 +31,25 @@ void kernel_main() {
     uint32_t l1_read_addr = base_l1_read_addr_0;
     noc_async_read_one_packet_set_state(noc_addr_0, input0_stride);
     for (uint32_t i = 0; i < input0_num_tiles_height; i++) {
-        noc_async_read_one_packet_with_state<true>(l1_read_addr, l1_write_addr);
-        l1_read_addr += input0_stride;
-        l1_write_addr += input0_stride;
-        l1_write_addr += input1_stride;  // advance to next row by passing input1 tiles
+        for (uint32_t j = 0; j < groups; j++) {
+            noc_async_read_one_packet_with_state<true>(l1_read_addr, l1_write_addr);
+            l1_read_addr += input0_stride;
+            l1_write_addr += input0_stride;
+            l1_write_addr += input1_stride;  // advance past the other input's tiles
+        }
     }
 
-    l1_write_addr = base_l1_write_addr + input0_num_tiles_width * tile_size;  // move pointer to end of first input
+    l1_write_addr = base_l1_write_addr + input0_stride;  // move pointer to end of first input
     l1_read_addr = base_l1_read_addr_1;
     noc_async_read_one_packet_set_state(noc_addr_1, input1_stride);
     for (uint32_t i = 0; i < input1_num_tiles_height; i++) {
-        DPRINT << "reading from " << l1_read_addr << " and writing to " << l1_write_addr << ENDL();
-        noc_async_read_one_packet_with_state<true>(l1_read_addr, l1_write_addr);
-        l1_read_addr += input1_stride;
-        l1_write_addr += input1_stride;
-        l1_write_addr += input0_stride;  // advance to next row by passing input0 tiles
+        for (uint32_t j = 0; j < groups; j++) {
+            DPRINT << "reading from " << l1_read_addr << " and writing to " << l1_write_addr << ENDL();
+            noc_async_read_one_packet_with_state<true>(l1_read_addr, l1_write_addr);
+            l1_read_addr += input1_stride;
+            l1_write_addr += input1_stride;
+            l1_write_addr += input0_stride;  // advance past the other input's tiles
+        }
     }
 
     noc_async_read_barrier();
