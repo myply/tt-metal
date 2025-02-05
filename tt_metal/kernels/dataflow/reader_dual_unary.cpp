eab2cb6d5bb54e99b5c324dd3d821a259773aa36
Anil Mahmud <amahmud@tenstorrent.com>
2025-02-05 03:50:45 +0000

    Cleaned reader and writer

--- tt_metal/kernels/dataflow/reader_dual_unary.cpp
+++ tt_metal/kernels/dataflow/reader_dual_unary.cpp
@@ -6,47 +6,35 @@
 
 #include "dataflow_api.h"
 
+inline void read_tiles(uint32_t num_tiles, uint32_t src_addr, uint32_t bank_id, uint32_t cb_id_in) {
+    // ublocks size defined in tiles
+    constexpr uint32_t ublock_size_tiles = 1;
+    uint32_t ublock_size_bytes = get_tile_size(cb_id_in) * ublock_size_tiles;
+
+    // read a ublock of tiles from src to CB, and then push the ublock to unpacker
+    for (uint32_t i = 0; i < num_tiles; i += ublock_size_tiles) {
+        uint64_t src_noc_addr = get_noc_addr_from_bank_id<true>(bank_id, src_addr);
+
+        cb_reserve_back(cb_id_in, ublock_size_tiles);
+        uint32_t l1_write_addr = get_write_ptr(cb_id_in);
+        noc_async_read(src_noc_addr, l1_write_addr, ublock_size_bytes);
+
+        noc_async_read_barrier();
+
+        cb_push_back(cb_id_in, ublock_size_tiles);
+        src_addr += ublock_size_bytes;
+    }
+}
+
 void kernel_main() {
     uint32_t src_addr_0 = get_arg_val<uint32_t>(0);
     uint32_t bank_id_0 = get_arg_val<uint32_t>(1);
     uint32_t src_addr_1 = get_arg_val<uint32_t>(2);
     uint32_t bank_id_1 = get_arg_val<uint32_t>(3);
-
     uint32_t num_tiles = get_arg_val<uint32_t>(4);
-
     constexpr uint32_t cb_id_in0 = 0;
     constexpr uint32_t cb_id_in1 = 1;
 
-    // ublocks size defined in tiles
-    constexpr uint32_t ublock_size_tiles = 1;
-    uint32_t ublock_size_bytes_0 = get_tile_size(cb_id_in0) * ublock_size_tiles;
-    uint32_t ublock_size_bytes_1 = get_tile_size(cb_id_in1) * ublock_size_tiles;
-
-    // read a ublock of tiles from src to CB, and then push the ublock to unpacker
-    for (uint32_t i = 0; i < num_tiles; i += ublock_size_tiles) {
-        uint64_t src_noc_addr = get_noc_addr_from_bank_id<true>(bank_id_0, src_addr_0);
-
-        cb_reserve_back(cb_id_in0, ublock_size_tiles);
-        uint32_t l1_write_addr = get_write_ptr(cb_id_in0);
-        noc_async_read(src_noc_addr, l1_write_addr, ublock_size_bytes_0);
-
-        noc_async_read_barrier();
-
-        cb_push_back(cb_id_in0, ublock_size_tiles);
-        src_addr_0 += ublock_size_bytes_0;
-    }
-
-    // read a ublock of tiles from src to CB, and then push the ublock to unpacker
-    for (uint32_t i = 0; i < num_tiles; i += ublock_size_tiles) {
-        uint64_t src_noc_addr = get_noc_addr_from_bank_id<true>(bank_id_1, src_addr_1);
-
-        cb_reserve_back(cb_id_in1, ublock_size_tiles);
-        uint32_t l1_write_addr = get_write_ptr(cb_id_in1);
-        noc_async_read(src_noc_addr, l1_write_addr, ublock_size_bytes_1);
-
-        noc_async_read_barrier();
-
-        cb_push_back(cb_id_in1, ublock_size_tiles);
-        src_addr_1 += ublock_size_bytes_1;
-    }
+    read_tiles(num_tiles, src_addr_0, bank_id_0, cb_id_in0);
+    read_tiles(num_tiles, src_addr_1, bank_id_1, cb_id_in1);
 }
--- tt_metal/kernels/dataflow/writer_dual_unary.cpp
+++ tt_metal/kernels/dataflow/writer_dual_unary.cpp
@@ -5,45 +5,34 @@
 #include "dataflow_api.h"
 #include "debug/dprint.h"
 
+inline void write_tiles(uint32_t num_tiles, uint32_t dst_addr, uint32_t bank_id, uint32_t cb_id_out) {
+    // single-tile ublocks
+    uint32_t ublock_size_tiles = 1;
+    uint32_t ublock_size_bytes = get_tile_size(cb_id_out);
+
+    for (uint32_t i = 0; i < num_tiles; i += ublock_size_tiles) {
+        uint64_t dst_noc_addr = get_noc_addr_from_bank_id<true>(bank_id, dst_addr);
+
+        cb_wait_front(cb_id_out, ublock_size_tiles);
+        uint32_t l1_read_addr = get_read_ptr(cb_id_out);
+        noc_async_write(l1_read_addr, dst_noc_addr, ublock_size_bytes);
+
+        noc_async_write_barrier();
+
+        cb_pop_front(cb_id_out, ublock_size_tiles);
+        dst_addr += ublock_size_bytes;
+    }
+}
+
 void kernel_main() {
     uint32_t dst_addr_0 = get_arg_val<uint32_t>(0);
     uint32_t bank_id_0 = get_arg_val<uint32_t>(1);
     uint32_t dst_addr_1 = get_arg_val<uint32_t>(2);
     uint32_t bank_id_1 = get_arg_val<uint32_t>(3);
-
     uint32_t num_tiles = get_arg_val<uint32_t>(4);
-
     constexpr uint32_t cb_id_out0 = tt::CBIndex::c_16;
     constexpr uint32_t cb_id_out1 = tt::CBIndex::c_17;
 
-    // single-tile ublocks
-    uint32_t ublock_size_bytes_0 = get_tile_size(cb_id_out0);
-    uint32_t ublock_size_bytes_1 = get_tile_size(cb_id_out1);
-    uint32_t ublock_size_tiles = 1;
-
-    for (uint32_t i = 0; i < num_tiles; i += ublock_size_tiles) {
-        uint64_t dst_noc_addr = get_noc_addr_from_bank_id<true>(bank_id_0, dst_addr_0);
-
-        cb_wait_front(cb_id_out0, ublock_size_tiles);
-        uint32_t l1_read_addr = get_read_ptr(cb_id_out0);
-        noc_async_write(l1_read_addr, dst_noc_addr, ublock_size_bytes_0);
-
-        noc_async_write_barrier();
-
-        cb_pop_front(cb_id_out0, ublock_size_tiles);
-        dst_addr_0 += ublock_size_bytes_0;
-    }
-
-    for (uint32_t i = 0; i < num_tiles; i += ublock_size_tiles) {
-        uint64_t dst_noc_addr = get_noc_addr_from_bank_id<true>(bank_id_1, dst_addr_1);
-
-        cb_wait_front(cb_id_out1, ublock_size_tiles);
-        uint32_t l1_read_addr = get_read_ptr(cb_id_out1);
-        noc_async_write(l1_read_addr, dst_noc_addr, ublock_size_bytes_1);
-
-        noc_async_write_barrier();
-
-        cb_pop_front(cb_id_out1, ublock_size_tiles);
-        dst_addr_1 += ublock_size_bytes_1;
-    }
+    write_tiles(num_tiles, dst_addr_0, bank_id_0, cb_id_out0);
+    write_tiles(num_tiles, dst_addr_1, bank_id_1, cb_id_out1);
 }
